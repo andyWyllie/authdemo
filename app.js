63095e8373a8f9c4522684e6c908150816b86cc7
Andy Wyllie <andy45386@gmail.com>
2018-04-01 03:13:55 +0000

    Adding secret route and updating ap.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/auth_demo_app");
 
 app.set('view engine', 'ejs');
 
 // home route
 app.get('/', function(req, res){
     res.render("home");
+});
+
+// secret route
+app.get('/secret', function(req, res){
+    res.render("secret");
 });
 
 // setting up the server
